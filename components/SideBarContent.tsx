e9656e010471668cf73b40c5d4d3247d902a9ac1
Henry S. Sjøen <hsjo@equinor.com>
2021-02-19 14:26:44 +0100

    Change description hint-label for the different cards and remove general information header

--- components/SideBarContent.tsx
+++ components/SideBarContent.tsx
@@ -10,131 +10,189 @@ import React from "react";
  * @constructor
  */
 export function SideBarContent(props: {
-  selectedObject: vsmObject,
-  onChangeName: (event: { target: { value: string } }) => void,
+  selectedObject: vsmObject;
+  onChangeName: (event: { target: { value: string } }) => void;
   onChangeRole: (event: { target: { value: string } }) => void;
   onChangeTime: (event: { target: { value: string } }) => void;
 }) {
   switch (props.selectedObject.vsmObjectType.pkObjectType) {
     case vsmObjectTypes.process:
-      return <>
-        <div style={{ paddingTop: 8 }}>
-          <TextField
-            label={"Title"}
-            multiline
-            rows={4}
-            variant={"default"}
-            value={props.selectedObject.name}
-            onChange={props.onChangeName}
-            id={"vsmObjectDescription"}
-          />
-        </div>
-      </>;
+      return (
+        <>
+          <div style={{ paddingTop: 8 }}>
+            <TextField
+              label={"Add title"}
+              multiline
+              rows={4}
+              variant={"default"}
+              value={props.selectedObject.name}
+              onChange={props.onChangeName}
+              id={"vsmObjectDescription"}
+            />
+          </div>
+        </>
+      );
     case vsmObjectTypes.supplier:
+      return (
+        <>
+          <div style={{ paddingTop: 8 }}>
+            <TextField
+              label={"Add supplier(s)"}
+              multiline
+              rows={4}
+              variant={"default"}
+              value={props.selectedObject.name}
+              onChange={props.onChangeName}
+              id={"vsmObjectDescription"}
+            />
+          </div>
+        </>
+      );
+
     case vsmObjectTypes.input:
+      return (
+        <>
+          <div style={{ paddingTop: 8 }}>
+            <TextField
+              label={"Add input(s)"}
+              multiline
+              rows={4}
+              variant={"default"}
+              value={props.selectedObject.name}
+              onChange={props.onChangeName}
+              id={"vsmObjectDescription"}
+            />
+          </div>
+        </>
+      );
+
     case vsmObjectTypes.output:
+      return (
+        <>
+          <div style={{ paddingTop: 8 }}>
+            <TextField
+              label={"Add output(s)"}
+              multiline
+              rows={4}
+              variant={"default"}
+              value={props.selectedObject.name}
+              onChange={props.onChangeName}
+              id={"vsmObjectDescription"}
+            />
+          </div>
+        </>
+      );
     case vsmObjectTypes.customer:
-      return <>
-        <div style={{ paddingTop: 8 }}>
-          <TextField
-            label={"Add description"}
-            multiline
-            rows={4}
-            variant={"default"}
-            value={props.selectedObject.name}
-            onChange={props.onChangeName}
-            id={"vsmObjectDescription"}
-          />
-        </div>
-      </>;
+      return (
+        <>
+          <div style={{ paddingTop: 8 }}>
+            <TextField
+              label={"Add customer(s)"}
+              multiline
+              rows={4}
+              variant={"default"}
+              value={props.selectedObject.name}
+              onChange={props.onChangeName}
+              id={"vsmObjectDescription"}
+            />
+          </div>
+        </>
+      );
     case vsmObjectTypes.mainActivity:
-      return <>
-        <div style={{ paddingTop: 8 }}>
-          <TextField
-            label={"Add description"}
-            multiline
-            rows={4}
-            variant={"default"}
-            value={props.selectedObject.name}
-            onChange={props.onChangeName}
-            id={"vsmObjectDescription"}
-          />
-        </div>
-      </>;
+      return (
+        <>
+          <div style={{ paddingTop: 8 }}>
+            <TextField
+              label={"Add description"}
+              multiline
+              rows={4}
+              variant={"default"}
+              value={props.selectedObject.name}
+              onChange={props.onChangeName}
+              id={"vsmObjectDescription"}
+            />
+          </div>
+        </>
+      );
     case vsmObjectTypes.subActivity:
-      return <>
-        <div style={{ paddingTop: 8 }}>
-          <TextField
-            label={"Add description"}
-            multiline
-            rows={4}
-            variant={"default"}
-            value={props.selectedObject.name}
-            onChange={props.onChangeName}
-            id={"vsmObjectDescription"}
+      return (
+        <>
+          <div style={{ paddingTop: 8 }}>
+            <TextField
+              label={"Add description"}
+              multiline
+              rows={4}
+              variant={"default"}
+              value={props.selectedObject.name}
+              onChange={props.onChangeName}
+              id={"vsmObjectDescription"}
+            />
+          </div>
+          <div style={{ paddingTop: 12 }}>
+            <TextField
+              label={"Role(s)"}
+              variant={"default"}
+              value={props.selectedObject.role?.toString()}
+              id={"vsmObjectRole"}
+              onChange={props.onChangeRole}
+            />
+            <div style={{ padding: 8 }} />
+            <TextField
+              label={"Duration"}
+              type={"number"}
+              meta={"Minutes"}
+              value={props.selectedObject.time?.toString()}
+              id={"vsmObjectTime"}
+              onChange={props.onChangeTime}
+            />
+          </div>
+          <div className={styles.sideBarSectionHeader}>
+            <p>Add problem, idea or question</p>
+          </div>
+          <SingleSelect
+            disabled
+            items={[
+              "Problem",
+              "Idea",
+              "Question",
+              "Existing Problem",
+              "Existing Idea",
+              "Existing Question",
+            ]}
+            handleSelectedItemChange={() => {}}
+            label="Select type"
           />
-        </div>
-        <div style={{ paddingTop: 12 }}>
+        </>
+      );
+    case vsmObjectTypes.waiting:
+      return (
+        <>
           <TextField
-            label={"Role"}
-            variant={"default"}
-            value={props.selectedObject.role?.toString()}
-            id={"vsmObjectRole"}
-            onChange={props.onChangeRole}
-          />
-          <div style={{ padding: 8 }} />
-          <TextField
-            type={"number"}
             label={"Duration"}
+            type={"number"}
             meta={"Minutes"}
             value={props.selectedObject.time?.toString()}
             id={"vsmObjectTime"}
             onChange={props.onChangeTime}
           />
-        </div>
-        <div className={styles.sideBarSectionHeader}>
-          <p>Add problem, idea or question</p>
-        </div>
-        <SingleSelect
-          disabled
-          items={[
-            "Problem",
-            "Idea",
-            "Question",
-            "Existing Problem",
-            "Existing Idea",
-            "Existing Question"
-          ]}
-          handleSelectedItemChange={() => {
-          }}
-          label="Select type"
-        />
-      </>;
-    case vsmObjectTypes.waiting:
-      return <>
-        <TextField
-          type={"number"}
-          label={"Duration"}
-          meta={"Minutes"}
-          value={props.selectedObject.time?.toString()}
-          id={"vsmObjectTime"}
-          onChange={props.onChangeTime}
-        />
-      </>;
+        </>
+      );
     case vsmObjectTypes.choice:
-      return <>
-        <div style={{ paddingTop: 8 }}>
-          <TextField
-            label={"Title"}
-            multiline
-            rows={4}
-            variant={"default"}
-            value={props.selectedObject.name}
-            onChange={props.onChangeName}
-            id={"vsmObjectDescription"}
-          />
-        </div>
-      </>;
+      return (
+        <>
+          <div style={{ paddingTop: 8 }}>
+            <TextField
+              label={"Define choice"}
+              multiline
+              rows={4}
+              variant={"default"}
+              value={props.selectedObject.name}
+              onChange={props.onChangeName}
+              id={"vsmObjectDescription"}
+            />
+          </div>
+        </>
+      );
     default:
       return <p>Invalid process type</p>;
   }
--- components/VSMSideBar.tsx
+++ components/VSMSideBar.tsx
@@ -4,23 +4,29 @@ import { SideBarContent } from "./SideBarContent";
 import React from "react";
 import { defaultObject } from "./VSMCanvas";
 
-export function VSMSideBar(props: { selectedObject: vsmObject, onChangeName: (event: { target: { value: any } }) => void, onChangeRole: (event) => void, onChangeTime: (event) => void }) {
-  return <div
-    className={
-      props.selectedObject === defaultObject
-        ? styles.hideSideBarToRight
-        : styles.vsmSideMenu
-    }
-  >
-    <h1 className={styles.sideBarHeader}>{props.selectedObject.vsmObjectType.name}</h1>
-    <div className={styles.sideBarSectionHeader}>
-      <p>General Information</p>
+export function VSMSideBar(props: {
+  selectedObject: vsmObject;
+  onChangeName: (event: { target: { value: any } }) => void;
+  onChangeRole: (event) => void;
+  onChangeTime: (event) => void;
+}) {
+  return (
+    <div
+      className={
+        props.selectedObject === defaultObject
+          ? styles.hideSideBarToRight
+          : styles.vsmSideMenu
+      }
+    >
+      <h1 className={styles.sideBarHeader}>
+        {props.selectedObject.vsmObjectType.name}
+      </h1>
+      <SideBarContent
+        selectedObject={props.selectedObject}
+        onChangeName={props.onChangeName}
+        onChangeRole={props.onChangeRole}
+        onChangeTime={props.onChangeTime}
+      />
     </div>
-    <SideBarContent
-      selectedObject={props.selectedObject}
-      onChangeName={props.onChangeName}
-      onChangeRole={props.onChangeRole}
-      onChangeTime={props.onChangeTime}
-    />
-  </div>;
+  );
 }
