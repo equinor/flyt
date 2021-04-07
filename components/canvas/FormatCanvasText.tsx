1e3a24a49d0fe9779a72cf3781b37a69723ebbff
Henry Skorpe Sjøen <hsjo@equinor.com>
2021-04-07 13:04:03 +0200

    Add default message if no QIPs added (#20)

--- components/canvas/FormatCanvasText.tsx
+++ components/canvas/FormatCanvasText.tsx
@@ -1,8 +1,8 @@
 export function formatCanvasText(text: string | null, maxLength = 120): string {
-  if (!text) return '';
+  if (!text) return "";
   let formattedText = text;
   if (text.length > maxLength) {
     formattedText = `${text?.slice(0, maxLength)}...`;
   }
-  return formattedText.replace(/(\r\n|\n|\r)/gm, ' ');
+  return formattedText.replace(/(\r\n|\n|\r)/gm, " ");
 }
