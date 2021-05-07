7e1f58251065101cc55b89f72c7a206bdf9f4844
Henry S. Sjøen <hsjo@equinor.com>
2021-05-07 15:20:18 +0200

    PixiJS V6 upgrade

--- components/canvas/entities/TextCircle.tsx
+++ components/canvas/entities/TextCircle.tsx
@@ -9,15 +9,13 @@ export function TextCircle(text = "?", color = 0x000000): PIXI.Container {
   circle.pivot.set(-circle.width / 2, -circle.height / 2);
 
   const circleText = new PIXI.Text(text, {
-    styleName: "Input/Label",
     fontFamily: "Equinor",
     fontSize: 12,
     fontStyle: "normal",
-    fontWeight: 500,
+    fontWeight: "500",
     lineHeight: 16,
     letterSpacing: 0,
     fill: "white",
-    fontColor: "white",
   });
   circleText.resolution = 4;
   // Center text
