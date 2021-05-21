c9b9616e60ac19584e26f6fb850354cb9bcdd8f3
Henry Sjøen <hsjo@equinor.com>
2021-05-21 15:29:47 +0200

    u

--- services/setUpSignalRConnection.tsx
+++ services/setUpSignalRConnection.tsx
@@ -1,12 +1,20 @@
 import * as signalR from "@microsoft/signalr";
+import { HubConnection } from "@microsoft/signalr";
+import getConfig from "next/config";
 
 /**
  * setUpSignalRConnection
+ * Connects to a project-hub.
+ * Use the returned connection to listen for changes etc...
  * @param projectId
+ * @returns HubConnection
  */
-export const setUpSignalRConnection = async (projectId: number) => {
+export const setUpSignalRConnection = async (
+  projectId: number
+): Promise<HubConnection> => {
+  const { publicRuntimeConfig } = getConfig();
   const connection = new signalR.HubConnectionBuilder()
-    .withUrl("https://vsm-api-dev.azurewebsites.net/vsmhub")
+    .withUrl(publicRuntimeConfig.API_HUB_URL)
     // .withUrl(Endpoint, { accessTokenFactory: () => getAccessToken() })
     .withAutomaticReconnect()
     .build();
@@ -15,6 +23,12 @@ export const setUpSignalRConnection = async (projectId: number) => {
     .start()
     .then(() => connection.invoke("SubscribeToVsm", projectId))
     .catch((err) => console.error(err));
+
+  connection.onreconnecting((error) =>
+    console.log("Trying to reconnect...", { error })
+  );
+  connection.onreconnected((s) => console.log("Reconnected!", s));
+  connection.onclose((error) => console.log("Connection closed.", { error }));
 
   return connection;
 };
