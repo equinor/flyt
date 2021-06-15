8adf5d7ea71d68f0b3e159f3b9a49bb98aa49418
Henry Sjøen <hsjo@equinor.com>
2021-06-15 14:06:08 +0200

    VSM-147 | Always show title in generic cards
    https://equinor-sds-si.atlassian.net/browse/VSM-147
    
    Når jeg skriver i input, customer, output, supplier lappene forsvinner også overskriftene.
    
    Fix: Tittel skal forsvinne for MainActivity og SubActivity, men vi skal alltid vise tittel for input, customer, output og supplier. Fikser dette også.

--- components/canvas/utils/assetGenerators/CreateGenericCardAsset.ts
+++ components/canvas/utils/assetGenerators/CreateGenericCardAsset.ts
@@ -1,20 +1,29 @@
 import * as PIXI from "pixi.js";
 import { getDefaultTextSprite } from "./GetDefaultTextSprite";
 import { vsmObject } from "../../../../interfaces/VsmObject";
+import { defaultTextStyle } from "../AssetFactory";
 
 export function createGenericCardAsset(
   vsmObject: vsmObject,
-  placeholder = "",
+  title = "",
   textResolution: number
 ) {
-  const { generic } = PIXI.Loader.shared.resources;
-  const textSprite = getDefaultTextSprite(
-    vsmObject,
-    100,
-    placeholder,
-    textResolution
-  );
   const wrapper = new PIXI.Container();
-  wrapper.addChild(new PIXI.Sprite(generic.texture), textSprite);
+
+  const { generic } = PIXI.Loader.shared.resources;
+  wrapper.addChild(new PIXI.Sprite(generic.texture));
+
+  if (title) {
+    const titleSprite = new PIXI.Text(title, defaultTextStyle);
+    titleSprite.alpha = 0.4;
+    titleSprite.y = 8;
+    titleSprite.x = 12;
+    wrapper.addChild(titleSprite);
+  }
+
+  const textSprite = getDefaultTextSprite(vsmObject, 100, null, textResolution);
+  textSprite.y = 21;
+  wrapper.addChild(textSprite);
+
   return wrapper;
 }
