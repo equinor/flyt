bad4326e9f9a1b8664fdbee2ca9977644f057dca
Henry S. Sjøen <hsjo@equinor.com>
2021-07-28 13:24:26 +0200

    Null check on the secret table page

--- components/taskTable.tsx
+++ components/taskTable.tsx
@@ -10,19 +10,21 @@ export default function TaskTable({
     <table>
       <thead>
         <tr>
-          {Object.keys(tasks[0]).map((k) => {
-            return <th key={k.toString()}>{k}</th>;
-          })}
+          {tasks &&
+            tasks[0] &&
+            Object.keys(tasks[0]).map((k) => {
+              return <th key={k.toString()}>{k}</th>;
+            })}
         </tr>
       </thead>
       <tbody>
-        {tasks.map((task) => {
+        {tasks?.map((task) => {
           return (
             <tr key={task.vsmTaskID}>
               {Object.keys(task).map((k) => {
                 return (
                   <td key={`${k}-${task.vsmTaskID}`}>
-                    {task && task[k] && task[k].toString()}
+                    {task[k] && task[k].toString()}
                   </td>
                 );
               })}
