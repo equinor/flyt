4f0dffde808a331366c11d52934fe84d7af3a078
Henry S. Sjøen <hsjo@equinor.com>
2021-07-30 16:08:11 +0200

    Hide projects with no name that I don't have access to
    But show your own projects with no name in both modes.

--- utils/categorizeProjects.tsx
+++ utils/categorizeProjects.tsx
@@ -1,6 +1,19 @@
 import { vsmProject } from "../interfaces/VsmProject";
 import { AccountInfo } from "@azure/msal-browser";
 
+export function checkIfCreatorOrEditor(
+  project: vsmProject,
+  account: AccountInfo
+) {
+  const imCreator =
+    project.created.userIdentity === account?.username.split("@")[0];
+
+  const imEditor = project.userAccesses?.find(
+    (a) => a.user === account?.username.split("@")[0]
+  );
+  return { imCreator, imEditor };
+}
+
 /**
  * Sort projects into two buckets.
  * 1. projects the account can Edit
@@ -19,12 +32,7 @@ export function categorizeProjects(
   const projectsICanView = [];
 
   projects?.forEach((project) => {
-    const imCreator =
-      project.created.userIdentity === account?.username.split("@")[0];
-
-    const imEditor = project.userAccesses?.find(
-      (a) => a.user === account?.username.split("@")[0]
-    );
+    const { imCreator, imEditor } = checkIfCreatorOrEditor(project, account);
 
     if (imCreator || imEditor) {
       projectsICanEdit.push(project);
