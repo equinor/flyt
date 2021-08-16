12bfa5ec4954bda95a32f8a7b4d813297b7b7574
Henry S. Sjøen <hsjo@equinor.com>
2021-08-16 13:54:24 +0200

    custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function ErrorNotFound(): JSX.Element {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>Error 404</h1>
+      <p>{`We couldn't find what you were looking for`}</p>
+    </div>
+  );
+}
