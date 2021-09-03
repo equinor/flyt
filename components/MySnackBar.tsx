6266b5b68cfa8f1fb45354dc7e007ef371edc1f1
Henry Sjoen <hsjo@equinor.com>
2021-09-03 12:41:20 +0200

    fix(snackmessage): display notifications on top over the sidesheet
    Change message visible regardless of having side bar open
    
    fix #130

--- components/MySnackBar.tsx
+++ components/MySnackBar.tsx
@@ -26,6 +26,7 @@ export const MySnackBar = (props: {
         borderRadius: 4,
         boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
         color: "white",
+        zIndex: 1000,
       }}
     >
       {props.children}
