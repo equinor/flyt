9c9f5a831f2cbc72037c8268d235fe3cd3068e2c
Henry S. Sjøen <hsjo@equinor.com>
2021-09-27 13:55:08 +0200

    fix hover and active color in sideNavBar

--- components/SideNavBarElement.tsx
+++ components/SideNavBarElement.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Icon, Tooltip } from "@equinor/eds-core-react";
 import Link from "next/link";
 import styles from "./SideNavBarElement.module.scss";
@@ -11,23 +11,12 @@ export default function SideNavBarElement(props: {
 }): JSX.Element {
   const router = useRouter();
   const path = router.asPath;
-  const [isHovered, setIsHovered] = useState(false);
 
   const isOnCurrentPage = path == props.href;
-
-  const getBackgroundColor = () => {
-    if (isOnCurrentPage) return "#E6FAEC";
-    if (isHovered) return "#DADADA";
-    return null;
-  };
   return (
     <div
-      style={{ backgroundColor: getBackgroundColor() }}
+      style={{ backgroundColor: isOnCurrentPage && "#E6FAEC" }}
       className={styles.iconContainer}
-      onMouseEnter={() => {
-        setIsHovered(true);
-      }}
-      onMouseLeave={() => setIsHovered(false)}
     >
       <Tooltip title={props.title} placement="right">
         <Link href={props.href}>
