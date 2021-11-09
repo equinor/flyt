baa2b0d6700a4acbade0ccd5c9a045573fec04db
Henry Skorpe Sjøen <hsjo@equinor.com>
2021-11-09 10:23:06 +0100

    fix: only show my projects in the myProjects tab (#287)
    The API change requires us to send in `userIds` as numbers instead of the `shortNames` we used before.
    Therefore, getting my `UserId` from another `api-call`
    
    fix #286

--- utils/createUrlParams.ts
+++ utils/createUrlParams.ts
@@ -3,21 +3,22 @@
  * @param params
  */
 export function createUrlParams(params: {
-  [x: string]: string | number | boolean;
+  [x: string]: string | number | boolean | Array<string | number | boolean>;
 }): string {
+  // If no params, return empty string
   if (!params) return "";
-  let first = true;
-  return Object.keys(params)
-    .map((key) => {
-      // Do not add the key-value-pair if the value is empty
-      if (!params[key]) return "";
 
-      // If this is the first key-value-pair added, prefix with "?".
-      const prefix = first ? `?` : `&`;
-      if (first) first = false;
-
-      // Return prefixed key with url-encoded values
-      return `${prefix}${key}=${encodeURIComponent(params[key])}`;
-    })
-    .join("");
+  // Create a string of params
+  return (
+    `?` +
+    Object.keys(params)
+      .map((key) => {
+        const value = params[key];
+        if (Array.isArray(value)) {
+          return value.map((v) => `${key}=${v}`).join("&");
+        }
+        return `${key}=${value}`;
+      })
+      .join("&")
+  );
 }
