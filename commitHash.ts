950702fd955cee3bf714adee52b20e8688b196ad
Dan André Svendsen <dsven@equinor.com>
2022-03-25 15:07:40 +0100

    Customize sidebar and markdown editor

--- commitHash.ts
+++ commitHash.ts
@@ -1,3 +1,3 @@
-export const commitHash = "a819fe7";
+export const commitHash = "099ebe7";
 export const commitHashUrl =
-  "https://github.com/equinor/MAD-VSM-WEB/commits/a819fe732e4ff43e9a4f2b6e12e30a2ba103dda8";
+  "https://github.com/equinor/MAD-VSM-WEB/commits/099ebe7930560eb0671b44ef843040bf328c5199";
