2560115b7249fdce6a85b5030f6b40ac1e8a1039
Dan André Svendsen <dsven@equinor.com>
2022-04-11 15:03:51 +0200

    Trying to select url in textarea

--- components/MarkdownEditor.tsx
+++ components/MarkdownEditor.tsx
@@ -1,10 +1,13 @@
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
-import { Button, Icon } from "@equinor/eds-core-react";
-import MDEditor, { commands } from "@uiw/react-md-editor";
-import React, { useState } from "react";
-import { check } from "@equinor/eds-icons";
-import { Typography } from "@equinor/eds-core-react";
+import { Button, Icon, Label } from "@equinor/eds-core-react";
+import MDEditor, {
+  ICommand,
+  TextAreaTextApi,
+  TextState,
+} from "@uiw/react-md-editor";
+import React, { useRef, useState } from "react";
+import { check, link } from "@equinor/eds-icons";
 import rehypeSanitize from "rehype-sanitize";
 
 export default function MarkdownEditor(props: {
@@ -17,22 +20,68 @@ export default function MarkdownEditor(props: {
   const { canEdit, defaultValue, id, label = "", onChange } = props;
   const [editMode, setEditMode] = useState(false);
   const [value, setValue] = useState(defaultValue);
+  //const [selection, setSelection] = useState({ start: 0, end: 0 });
+
+  // const set = () => (, end, what) {
+  //   return this.substring(0, start) + what + this.substring(end);
+  // };
+
+  //console.log("The Hello World Code!".replaceBetween(4, 9, "Hi"));
+  // useEffect(() => {
+  //   const txtarea = document.getElementById(
+  //     "markdown-editor-textarea"
+  //   ) as HTMLTextAreaElement;
+  //   //txtarea.focus();
+  //   txtarea.setSelectionRange(0, 20);
+  // }, [selection]);
+  const ref = useRef<any>();
+
+  const markdownLink: ICommand = {
+    name: "Markdown link",
+    keyCommand: "markdownLink",
+    buttonProps: { "aria-label": "Markdown link" },
+    icon: <Icon data={link} color={"#007079"} size={24} />,
+    execute: (state: TextState, api: TextAreaTextApi) => {
+      const startText = state.text.substring(0, state.selection.start);
+      const linkText = state.selectedText ? state.selectedText : "text";
+      const endText = state.text.substring(
+        state.selection.end,
+        state.text.length
+      );
+      const modifyText = `${startText}[${linkText}](url)${endText}`;
+      onChange(modifyText);
+      setValue(modifyText);
+      debugger;
+      ref.current.textarea.setSelectionRange(
+        state.selection.start + state.selectedText.length,
+        state.selection.end + 3
+      );
+      //api.setSelectionRange({});
+      //api.replaceSelection(modifyText);
+      //onChange(state.text);
+      //setValue(state.text);
+      //api.setSelectionRange(state.selection);
+    },
+  };
 
   return (
     <div id={id} data-color-mode="light">
-      <label htmlFor="mdEditor" style={{ marginLeft: "8px" }}>
+      {/* <label htmlFor="mdEditor" style={{ marginLeft: "8px" }}>
         <Typography
           color={canEdit ? "#6F6F6F" : "rgba(190,190,190,1)"}
           group="input"
           variant="label"
+          //style={{ margin: "0 8px" }}
         >
           {label}
         </Typography>
-      </label>
+      </label> */}
+      <Label htmlFor="mdEditor" label={label}></Label>
       <div style={{ display: "flex", gap: 12 }}>
         <div onClick={() => canEdit && setEditMode(true)} style={{ flex: 1 }}>
           <MDEditor
             id="mdEditor"
+            ref={ref}
             value={value}
             onChange={(value?: string) => {
               onChange(value);
@@ -40,7 +89,7 @@ export default function MarkdownEditor(props: {
             }}
             preview={editMode ? "edit" : "preview"}
             extraCommands={[]}
-            commands={[commands.link]}
+            commands={[markdownLink]}
             hideToolbar={!editMode}
             previewOptions={{
               rehypePlugins: [[rehypeSanitize]],
@@ -56,6 +105,7 @@ export default function MarkdownEditor(props: {
               linkTarget: "_blank",
             }}
             textareaProps={{
+              id: "markdown-editor-textarea",
               onFocus: (e) => {
                 e.target.setSelectionRange(
                   e.target.value.length,
