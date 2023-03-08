64d88e36b94476118be8bdd8eeb4e2e05e1aaff1
Adrian Nesvik <adrian.nesvik@outlook.com>
2023-03-08 11:12:45 +0100

    fix: qipr layout

--- components/canvas/NodeTypes/QIPRContainer.tsx
+++ components/canvas/NodeTypes/QIPRContainer.tsx
@@ -4,18 +4,35 @@ import { getTaskColor } from "utils/getTaskColor";
 import { TextCircle } from "../entities/TextCircle";
 import { taskObject } from "interfaces/taskObject";
 
+const sortOrder = ["Problem", "Question", "Idea", "Risk"];
+
 export const QIPRContainer = (props: {
   onClick?(): void;
   tasks: taskObject[];
 }) => (
-  <div className={styles.QIPRContainer} onClick={props.onClick}>
-    {props.tasks.map((task, index) => (
-      <div
-        key={index}
-        style={{ margin: 2.5, marginLeft: index >= 4 ? 0 : 2.5 }}
-      >
-        <TextCircle text={""} color={getTaskColor(task)} />
-      </div>
-    ))}
+  <div
+    className={styles.QIPRContainer}
+    onClick={props.onClick}
+    style={{
+      width: (((props.tasks?.length / 4) >> 0) + 1) * 33,
+    }}
+  >
+    {props.tasks
+      .sort((a, b) =>
+        a.type !== b.type
+          ? sortOrder.indexOf(a.type) - sortOrder.indexOf(b.type)
+          : a.number - b.number
+      )
+      .map((task, index) => (
+        <div
+          key={index}
+          style={{ margin: 2.5, marginLeft: index >= 4 ? 0 : 2.5 }}
+        >
+          <TextCircle
+            text={task.type.slice(0, 1) + (task.number + 1)}
+            color={getTaskColor(task)}
+          />
+        </div>
+      ))}
   </div>
 );
