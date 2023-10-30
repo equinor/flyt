45eb95b2da7e93c201007cd4fd8ba3e6b34570d1
Dan André Svendsen <dsven@equinor.com>
2023-10-30 12:44:42 +0100

    chore: Remove unused file

--- nextConfig.ts
+++
@@ -1,9 +0,0 @@
-import getConfig from "next/config";
-
-const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
-export const nextConfig = {
-  serverRuntimeConfig: {
-    AUTH_SECRET: serverRuntimeConfig.AUTH_SECRET, // "authentication secret or public key, used for validating user requests in pages/api",
-    AUDIENCE: serverRuntimeConfig.AUDIENCE,
-  },
-};
