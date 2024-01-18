4f093423b9e9e3c7b350835f01d49359603ed4d3
Adrian Nesvik <adrian.nesvik@outlook.com>
2024-01-18 13:38:14 +0100

    fix: Disable rendering of empty QIPR container

--- components/canvas/QIPRContainer.tsx
+++ components/canvas/QIPRContainer.tsx
@@ -6,29 +6,30 @@ import { Task } from "types/Task";
 import { getTaskShorthand } from "utils/getTaskShorthand";
 import { taskSorter } from "utils/taskSorter";
 
-export const QIPRContainer = (props: { onClick?(): void; tasks: Task[] }) => (
-  <div
-    className={styles.QIPRContainer}
-    onClick={props.onClick}
-    style={{
-      width: getQIPRContainerWidth(props.tasks),
-    }}
-  >
-    {props.tasks
-      ?.filter((task) => !task.solved)
-      .sort(taskSorter())
-      .map((task, index) => {
-        return (
-          <div
-            key={index}
-            style={{ margin: 2.5, marginLeft: index >= 4 ? 0 : 2.5 }}
-          >
-            <TextCircle
-              text={getTaskShorthand(task)}
-              color={getTaskColor(task)}
-            />
-          </div>
-        );
-      })}
-  </div>
-);
+export const QIPRContainer = (props: { onClick?(): void; tasks: Task[] }) =>
+  props.tasks?.length > 0 && (
+    <div
+      className={styles.QIPRContainer}
+      onClick={props.onClick}
+      style={{
+        width: getQIPRContainerWidth(props.tasks),
+      }}
+    >
+      {props.tasks
+        ?.filter((task) => !task.solved)
+        .sort(taskSorter())
+        .map((task, index) => {
+          return (
+            <div
+              key={index}
+              style={{ margin: 2.5, marginLeft: index >= 4 ? 0 : 2.5 }}
+            >
+              <TextCircle
+                text={getTaskShorthand(task)}
+                color={getTaskColor(task)}
+              />
+            </div>
+          );
+        })}
+    </div>
+  );
