3b8299e830a54eb75ceaf90826f293403c539afa
Adrian Nesvik <adrian.nesvik@outlook.com>
2024-02-19 15:24:31 +0100

    fix: Resolve error moving choice within a loop

--- components/canvas/utils/targetIsInSubtree.tsx
+++ components/canvas/utils/targetIsInSubtree.tsx
@@ -9,14 +9,21 @@ export const targetIsInSubtree = (
 ) => {
   if (node.data.columnId !== target.data.columnId) return false;
   if (originalNodeId !== node.id) {
-    // Prevents infinite loops
     for (let i = 0; i < node.data.children.length; i++) {
       const id = node.data.children[i];
       if (id === target.id) {
         return true;
       } else {
         const nextNode = nodes.find((node) => node.id === id);
-        if (nextNode && targetIsInSubtree(nextNode, target, nodes, node.id)) {
+        if (
+          nextNode &&
+          targetIsInSubtree(
+            nextNode,
+            target,
+            nodes,
+            !originalNodeId ? node.id : originalNodeId
+          )
+        ) {
           return true;
         }
       }
