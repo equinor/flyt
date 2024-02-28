408429fa6b59242fc44654724c4fab9c3deb7083
Adrian Nesvik <adrian.nesvik@outlook.com>
2024-02-28 10:43:41 +0100

    chore: Hardcoded service message

--- components/ServiceMessageBanner.tsx
+++ components/ServiceMessageBanner.tsx
@@ -1,21 +1,21 @@
-import { Banner, Button, Icon } from "@equinor/eds-core-react";
+import { Banner, Icon } from "@equinor/eds-core-react";
 import React from "react";
 import { high_priority } from "@equinor/eds-icons";
-import { useQuery } from "react-query";
+// import { useQuery } from "react-query";
 import style from "./ServiceMessageBanner.module.scss";
-import { getServiceMessage } from "../services/CommonAPIService";
+// import { getServiceMessage } from "../services/CommonAPIService";
 import gfm from "remark-gfm";
 import ReactMarkdown from "react-markdown";
-import getConfig from "next/config";
+// import getConfig from "next/config";
 
 export const ServiceMessageBanner = () => {
-  const { publicRuntimeConfig } = getConfig();
-  const { data, isLoading, error } = useQuery("ServiceMessage", () =>
-    getServiceMessage(publicRuntimeConfig.ENVIRONMENT, "Flyt")
-  );
+  // const { publicRuntimeConfig } = getConfig();
+  // const { data, isLoading, error } = useQuery("ServiceMessage", () =>
+  //   getServiceMessage(publicRuntimeConfig.ENVIRONMENT, "Flyt")
+  // );
 
-  if (isLoading || error) return null;
-  if (data && !data.status) return null; // https://github.com/equinor/flyt/issues/259#issue-1034675684
+  // if (isLoading || error) return null;
+  // if (data && !data.status) return null; // https://github.com/equinor/flyt/issues/259#issue-1034675684
 
   return (
     <>
@@ -24,9 +24,12 @@ export const ServiceMessageBanner = () => {
           <Icon data={high_priority} />
         </Banner.Icon>
         <div style={{ display: "flex", alignItems: "center" }}>
-          <ReactMarkdown remarkPlugins={[gfm]}>{data?.message}</ReactMarkdown>
+          <ReactMarkdown remarkPlugins={[gfm]}>
+            Flyt 1.0 is now in read-only mode. You can view your data, but
+            changes can no longer be made.
+          </ReactMarkdown>
         </div>
-        {data?.urlString && (
+        {/* {data?.urlString && (
           <Banner.Actions>
             <a
               href={encodeURI(data.urlString)}
@@ -36,7 +39,7 @@ export const ServiceMessageBanner = () => {
               <Button variant={"ghost"}>Read more</Button>
             </a>
           </Banner.Actions>
-        )}
+        )} */}
       </Banner>
     </>
   );
