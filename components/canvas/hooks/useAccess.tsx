4dae6e1b0908804e2a14f1e399a387eb365d75f6
Srithulasi Lakshmi Vadde <srv@equinor.com>
2025-02-14 14:42:39 +0530

    feat: All roles combined into Contributor role (#897)
    - [x] Delete process confirmation text updated
    - [x] All three roles combined into contributor and have all the access
    which the owner, admin has access
    - [x]  Updated at all places where role plays a major role
    - [x]  Owner in the dropdown at process name has been removed.

--- components/canvas/hooks/useAccess.tsx
+++ components/canvas/hooks/useAccess.tsx
@@ -2,14 +2,15 @@ import { Project } from "@/types/Project";
 import { getMyAccess } from "@/utils/getMyAccess";
 import router from "next/router";
 import { useUserAccount } from "./useUserAccount";
+import { accessRoles } from "@/types/AccessRoles";
 
 export const useAccess = (project: Project) => {
   const { version } = router.query;
+  const { Contributor, Reader } = accessRoles;
   const account = useUserAccount();
   const myAccess = getMyAccess(project, account);
-  const userCanEdit = !version && myAccess !== "Reader";
-  const isAdmin = myAccess === "Admin" || myAccess === "Owner";
-
+  const userCanEdit = !version && myAccess !== Reader;
+  const isAdmin = myAccess === Contributor;
   return {
     myAccess,
     userCanEdit,
