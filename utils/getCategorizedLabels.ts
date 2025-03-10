d3908f38582f0550c74d57adcc3bb7191fc12045
srvEq <srv@equinor.com>
2025-03-10 15:55:27 +0530

    feat: Sorted labels alphabetically in each category

--- utils/getCategorizedLabels.ts
+++ utils/getCategorizedLabels.ts
@@ -3,8 +3,14 @@ import { ProcessLabel } from "@/types/ProcessLabel";
 
 export function getCategorizedLabels(labels: ProcessLabel[]) {
   const { l1LevelOrganisation, functionalArea, assets } = labelsCategories;
-
-  const filteredLabels = labels.reduce((acc: any, val: ProcessLabel) => {
+  const sortedLabels = labels.sort((label1, label2) => {
+    const labelA = label1.text.toUpperCase();
+    const labelB = label2.text.toUpperCase();
+    if (labelA < labelB) return -1;
+    else if (labelA > labelB) return 1;
+    return 0;
+  });
+  const filteredLabels = sortedLabels.reduce((acc: any, val: ProcessLabel) => {
     const { category } = val;
     if (!acc[category]) {
       acc[category] = [];
