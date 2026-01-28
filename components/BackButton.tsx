ae6a89b32add9686761e6a0877690a681223857b
Chetna Rana <chera@equinor.com>
2026-01-28 13:24:36 +0530

    fix: prevent back button overlap (#1067)

---
+++ components/BackButton.tsx
@@ -0,0 +1,16 @@
+import { Button, Icon } from "@equinor/eds-core-react";
+import { arrow_back_ios } from "@equinor/eds-icons";
+import { useRouter } from "next/router";
+
+export function BackButton(): JSX.Element {
+  const router = useRouter();
+
+  return (
+    <div style={{ position: "absolute", left: "14px" }}>
+      <Button variant="ghost" onClick={() => router.back()}>
+        <Icon data={arrow_back_ios} color={"#007079"} />
+        Back
+      </Button>
+    </div>
+  );
+}
