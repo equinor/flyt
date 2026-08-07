d773b442ac81d5e5e1b82a761962abfe87b361c3
Keshav Keshav <kkes@equinor.com>
2026-08-07 18:29:57 +0530

    app insights intrument key in env files

--- services/appInsights.ts
+++ services/appInsights.ts
@@ -1,11 +1,20 @@
 import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
 
+const appInsightsConnectionString =
+  process.env.NEXT_PUBLIC_APPINSIGHTS_CONNECTION_STRING?.trim();
+
+if (!appInsightsConnectionString) {
+  throw new Error(
+    "NEXT_PUBLIC_APPINSIGHTS_CONNECTION_STRING is required in environment variables"
+  );
+}
+
 export const reactPlugin = new ReactPlugin();
 
 export const appInsights = new ApplicationInsights({
   config: {
-    connectionString: process.env.NEXT_PUBLIC_APPINSIGHTS_CONNECTION_STRING,
+    connectionString: appInsightsConnectionString,
     extensions: [reactPlugin],
     enableAutoRouteTracking: true,
   },
