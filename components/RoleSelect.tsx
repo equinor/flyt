75817429a910ba4b0e27e0eaea97496db14f50a6
Satyam Choudhary <sacho@equinor.com>
2026-08-13 12:01:47 +0530

    fix: remove commented code

--- components/RoleSelect.tsx
+++ components/RoleSelect.tsx
@@ -9,18 +9,6 @@ export const RoleSelect = (props: {
   disabled: boolean;
 }) => {
   return (
-    // <select
-    //   defaultValue={props.defaultValue}
-    //   className={styles.roleSelect}
-    //   id="AccessRoles"
-    //   name="AccessRole"
-    //   onChange={(event) => props.onChange(event.target.value)}
-    //   disabled={props.disabled}
-    // >
-    //   <option value="Contributor">Contributor</option>
-    //   <option value="Remove">Remove</option>
-    // </select>
-
     <div
       className={[
         styles.container,
