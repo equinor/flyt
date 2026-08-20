04cf569aaf9f71543b3b450bf88ea114dd15c5e3
Keshav Keshav <kkes@equinor.com>
2026-08-20 12:41:06 +0530

    merge conf resolved

--- components/MandatoryInfoWizard/MandatoryInfoWizard.tsx
+++ components/MandatoryInfoWizard/MandatoryInfoWizard.tsx
@@ -13,11 +13,13 @@ type Props = {
   project: Project;
   onDiscard: () => void;
   updateProjectName: (name: string) => Promise<void>;
+  loading: boolean;
 };
 
 export function MandatoryInfoWizard({
   project,
   updateProjectName,
+  loading,
   onDiscard,
 }: Props) {
   const threshold = new Date();
@@ -146,6 +148,7 @@ export function MandatoryInfoWizard({
         {stage === "giveAccesses" && (
           <GiveAccessStage
             project={project}
+            loading={loading}
             processName={project.name ?? "Untitled Process"}
             onBack={moveBack}
             onNext={moveForward}
