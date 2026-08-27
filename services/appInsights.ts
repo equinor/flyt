6d4f0336406787626fddf29c8b930eecc036a341
Keshav Keshav <kkes@equinor.com>
2026-08-27 12:51:13 +0530

    fix instrument issue linting

--- services/appInsights.ts
+++ services/appInsights.ts
@@ -1,15 +1,6 @@
 import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
 
-const appInsightsConnectionString =
-  process.env.NEXT_PUBLIC_APPINSIGHTS_CONNECTION_STRING;
-
-if (!appInsightsConnectionString) {
-  console.error(
-    "Application Insights connection string is not defined. Please set the NEXT_PUBLIC_APPINSIGHTS_CONNECTION_STRING environment variable."
-  );
-}
-
 const reactPlugin = new ReactPlugin();
 export const appInsights = new ApplicationInsights({
   config: {
