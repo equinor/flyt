3e95aa5f3d3b7398c04966328bb1c61db3a70c1e
Keshav Keshav <kkes@equinor.com>
2026-08-27 12:56:00 +0530

    fix instrument issue linting

--- services/appInsights.ts
+++ services/appInsights.ts
@@ -4,7 +4,7 @@ import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
 const reactPlugin = new ReactPlugin();
 export const appInsights = new ApplicationInsights({
   config: {
-    instrumentationKey: process.env.NEXT_PUBLIC_APPINSIGHTS_CONNECTION_STRING,
+    instrumentationKey: process.env.NEXT_PUBLIC_INSTRUMENTATION_KEY,
     extensions: [reactPlugin],
     enableAutoRouteTracking: true,
   },
