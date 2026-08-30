b83e69b1db094eb6d41d6032e4a1d28107c68c58
Keshav Keshav <kkes@equinor.com>
2026-08-30 20:50:10 +0530

    appinsights config chnages

--- services/appInsights.ts
+++ services/appInsights.ts
@@ -4,12 +4,16 @@ import getConfig from "next/config";
 const reactPlugin = new ReactPlugin();
 const { publicRuntimeConfig } = getConfig();
 const instrumentationKey = publicRuntimeConfig.INSTRUMENTATION_KEY;
-export const appInsights = new ApplicationInsights({
-  config: {
-    instrumentationKey,
-    extensions: [reactPlugin],
-    enableAutoRouteTracking: true,
-  },
-});
+
+export const appInsights =
+  typeof window !== "undefined" && instrumentationKey
+    ? new ApplicationInsights({
+        config: {
+          instrumentationKey,
+          extensions: [reactPlugin],
+          enableAutoRouteTracking: true,
+        },
+      })
+    : undefined;
 
 appInsights?.loadAppInsights();
