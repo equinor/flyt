5100b726bb335c72bbc5ce969ff2c7b288c643e8
Keshav Keshav <kkes@equinor.com>
2026-08-31 19:43:38 +0530

    testing commit

--- services/appInsights.ts
+++ services/appInsights.ts
@@ -2,8 +2,8 @@ import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
 import { AppInsightsConfig } from "@/Config";
 const reactPlugin = new ReactPlugin();
-const key = AppInsightsConfig.connectionString;
-
+const key =
+  "InstrumentationKey=fe5e08f6-3b9d-4513-8f9c-eedf1f931267;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;ApplicationId=2025dc43-1ce3-4c5d-9c44-850f5a549c1e";
 export const appInsights = key
   ? new ApplicationInsights({
       config: {
