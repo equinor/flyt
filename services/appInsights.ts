2db6a166ea4e076d32ae5a32e81ae111a1c33a60
Keshav Keshav <kkes@equinor.com>
2026-08-31 17:39:41 +0530

    added a null check

--- services/appInsights.ts
+++ services/appInsights.ts
@@ -4,12 +4,17 @@ import { AppInsightsConfig } from "@/Config";
 const reactPlugin = new ReactPlugin();
 const key = AppInsightsConfig.connectionString;
 
-export const appInsights = new ApplicationInsights({
-  config: {
-    connectionString: key,
-    extensions: [reactPlugin],
-    enableAutoRouteTracking: true,
-  },
-});
-console.log("Connection String Exists:", !!key);
-appInsights?.loadAppInsights();
+export const appInsights = key
+  ? new ApplicationInsights({
+      config: {
+        connectionString: key,
+        extensions: [reactPlugin],
+        enableAutoRouteTracking: true,
+      },
+    })
+  : undefined;
+if (!appInsights) {
+  console.warn("Application Insights connection string not configured");
+} else {
+  appInsights.loadAppInsights();
+}
