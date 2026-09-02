5dbecbb373613df141b68ed9b13127bff3541226
Keshav Keshav <kkes@equinor.com>
2026-09-02 19:42:06 +0530

    instrument congif changes in publicruntime

--- services/appInsights.ts
+++ services/appInsights.ts
@@ -1,10 +1,12 @@
 import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
 import { getAppInsightsInstrumentationKey } from "@/appInsightConfig";
+import getConfig from "next/config";
 
+const { publicRuntimeConfig } = getConfig();
 const reactPlugin = new ReactPlugin();
 const instrumentationKey = getAppInsightsInstrumentationKey(
-  process.env.ENVIRONMENT
+  publicRuntimeConfig.ENVIRONMENT
 );
 
 export const appInsights = instrumentationKey
