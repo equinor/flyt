c6f65dcf9c02623fe08db86222320d90dfc127ce
Keshav Keshav <kkes@equinor.com>
2026-09-03 13:56:53 +0530

    fix:instrument changes removed from env variables and added from config (#1166)

--- services/appInsights.ts
+++ services/appInsights.ts
@@ -1,18 +1,22 @@
 import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
+import { getAppInsightsInstrumentationKey } from "@/appInsightConfig";
+import getConfig from "next/config";
 
+const { publicRuntimeConfig } = getConfig();
 const reactPlugin = new ReactPlugin();
-const instrumentationKey = process.env.NEXT_PUBLIC_INSTRUMENTATION_KEY;
+const instrumentationKey = getAppInsightsInstrumentationKey(
+  publicRuntimeConfig.ENVIRONMENT
+);
 
-export const appInsights =
-  typeof window !== "undefined" && instrumentationKey
-    ? new ApplicationInsights({
-        config: {
-          instrumentationKey,
-          extensions: [reactPlugin],
-          enableAutoRouteTracking: true,
-        },
-      })
-    : undefined;
+export const appInsights = instrumentationKey
+  ? new ApplicationInsights({
+      config: {
+        instrumentationKey,
+        extensions: [reactPlugin],
+        enableAutoRouteTracking: true,
+      },
+    })
+  : undefined;
 
 appInsights?.loadAppInsights();
