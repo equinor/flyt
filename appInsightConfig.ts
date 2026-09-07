e779e291dbd6005a99bba74d94789f90d997e021
Keshav Keshav <kkes@equinor.com>
2026-09-07 11:40:51 +0530

    seperate instrument keys added

--- appInsightConfig.ts
+++ appInsightConfig.ts
@@ -1,11 +1,9 @@
 type AppInsightsEnvironment = "DEV" | "QA" | "PROD" | "TEST";
-const dev = "fe5e08f6-3b9d-4513-8f9c-eedf1f931267";
-const prod = "b01f1bf9-4bf2-494f-bd3c-986517dc4dd1";
 const instrumentationKeys: Record<AppInsightsEnvironment, string> = {
-  DEV: dev,
-  QA: dev,
-  PROD: prod,
-  TEST: dev,
+  DEV: "fe5e08f6-3b9d-4513-8f9c-eedf1f931267",
+  QA: "f1516c0b-cb49-4643-b5d8-fb09c7663792",
+  PROD: "b01f1bf9-4bf2-494f-bd3c-986517dc4dd1",
+  TEST: "f7073bc9-143b-46e2-8380-fe6b656837e8",
 };
 
 export const getAppInsightsInstrumentationKey = (
