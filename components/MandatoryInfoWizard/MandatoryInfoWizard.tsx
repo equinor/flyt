03a0fdd5f21f922e8a5a74ddceb701aa5854fd0b
Keshav Keshav <kkes@equinor.com>
2026-09-15 19:28:06 +0530

    fix:name consistent issue in addlabel stage (#1178)

--- components/MandatoryInfoWizard/MandatoryInfoWizard.tsx
+++ components/MandatoryInfoWizard/MandatoryInfoWizard.tsx
@@ -136,6 +136,7 @@ export function MandatoryInfoWizard({
       >
         {stage === "addName" && (
           <AddNameStage
+            processName={newName}
             onNameChange={setNewName}
             onNext={moveForward}
             onRequestDiscard={() => setShowDiscardConfirmation(true)}
